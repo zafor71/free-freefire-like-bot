c31f32ed8366952d6323dc7ae1cac38e1f02287c
zafor71 <idxbdff@gmail.com>
2025-07-03 20:34:57 +0600

    use new link api

--- cogs/likeCommands.py
+++ cogs/likeCommands.py
@@ -15,7 +15,7 @@ CONFIG_FILE = "like_channels.json"
 class LikeCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_host = "https://likeapixthug.onrender.com"
+        self.api_host = "https://free-fire-like1.p.rapidapi.com"
         self.config_data = self.load_config()
         self.cooldowns = {}
         self.session = aiohttp.ClientSession()

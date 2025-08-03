ea051f03d433c36d165cdebc8c951006a698e011
Abu Zafor <idxbdff@gmail.com>
2025-08-03 12:37:31 -0700

    Update likeCommands.py

--- cogs/likeCommands.py
+++ cogs/likeCommands.py
@@ -15,7 +15,7 @@ CONFIG_FILE = "like_channels.json"
 class LikeCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_host = "https://free-fire-like1.p.rapidapi.com"
+        self.api_host = "https://likexthug.vercel.app/like?uid={uid}&region={region}&key=GREAT"
         self.config_data = self.load_config()
         self.cooldowns = {}
         self.session = aiohttp.ClientSession()
